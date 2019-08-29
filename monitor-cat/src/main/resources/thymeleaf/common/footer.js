fb0991e4c7a67ddad17f178f7ef5ab34bfd63a8d
wei_yu <1455755530@qq.com>
2019-08-29 17:31:37 +0800

    fix and remove 去掉 BootStrap；添加 LayUI

--- monitor-cat/src/main/resources/thymeleaf/common/footer.js
+++ monitor-cat/src/main/resources/thymeleaf/common/footer.js
@@ -1,4 +1,1 @@
-<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossOrigin="anonymous"/>;
+<script th:src="@{../../static/layui.js}"/>;
