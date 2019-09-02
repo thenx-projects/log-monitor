7b2d6a82d804204326358d9bf960c79f7d5301b4
wei_yu <1455755530@qq.com>
2019-09-02 17:58:21 +0800

    fix: 页面调整、服务器相关信息获取

--- monitor-cat/src/main/java/com/tgpms/web/index/IndexController.java
+++ monitor-cat/src/main/java/com/tgpms/web/index/IndexController.java
@@ -1,10 +1,15 @@
 package com.tgpms.web.index;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author may
@@ -22,9 +27,40 @@ public class IndexController {
      *
      * @return null
      */
+    @ApiOperation(value = "log-monitor 首页", notes = "log-monitor 首页", httpMethod = "POST")
     @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST})
     public String index() {
         log.info("\n ----------> into the index method and page");
         return "index";
+    }
+
+    /**
+     * 拿到项目全路径
+     *
+     * @param request 请求域
+     * @return null
+     */
+    @ApiOperation(value = "拿到项目全路径", notes = "拿到项目全路径", httpMethod = "POST")
+    @ResponseBody
+    @PostMapping(value = "/serverPath")
+    public String serverPath(HttpServletRequest request) {
+        // 项目所在服务器全路径
+        String realPath = request.getServletContext().getRealPath("/");
+
+        // 请求地址
+        String servletPath = request.getServletPath();
+
+        // 服务器地址
+        String serverName = request.getServerName();
+
+        // 服务器端口
+        Integer serverPort = request.getServerPort();
+
+        // 项目名称
+        String contextPath = request.getContextPath();
+
+        // 请求全路径
+        String requestPath = request.getRequestURL().toString();
+        return realPath;
     }
 }
--- monitor-cat/src/main/java/com/tgpms/web/monitor/controller/CatLogsController.java
+++ monitor-cat/src/main/java/com/tgpms/web/monitor/controller/CatLogsController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
