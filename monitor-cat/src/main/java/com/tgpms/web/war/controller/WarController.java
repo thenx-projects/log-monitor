a7b343c940adb02de5fbea831eaba882a53f6556
wei_yu <1455755530@qq.com>
2019-09-05 17:10:35 +0800

    fix: 去掉无用包

--- monitor-cat/src/main/java/com/tgpms/web/war/controller/WarController.java
+++ monitor-cat/src/main/java/com/tgpms/web/war/controller/WarController.java
@@ -19,8 +19,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import static com.sun.xml.internal.fastinfoset.util.DuplicateAttributeVerifier.MAP_SIZE;
-
 /**
  * @author May
  * <p>
