53a4432294e4ae31218d26fc354eccd4a0312ae8
ganghuo <993610942@qq.com>
2019-09-05 16:54:55 +0800

    1.将monitor-common模块中的groupId更改为与父pom中的groupId 2.引入org.thenx.common包 3.父pom依赖管理中添加了monitor-common模块

---
+++ monitor-common/src/main/java/org/thenx/common/exception/ProjectException.java
@@ -0,0 +1,17 @@
+package org.thenx.common.exception;
+
+/**
+ * @author May
+ * <p>
+ * 全局统一管理异常
+ */
+public class ProjectException extends RuntimeException{
+
+    public ProjectException(String message) {
+        super(message);
+    }
+
+    public ProjectException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
