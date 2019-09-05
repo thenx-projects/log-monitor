ba87d71f6534388a624d387d49039dcb33dda401
wei_yu <1455755530@qq.com>
2019-09-05 16:46:31 +0800

    add tomcat 指令

---
+++ monitor-cat/src/main/java/com/tgpms/web/tomcat/core/TomcatControl.java
@@ -0,0 +1,63 @@
+package com.tgpms.web.tomcat.core;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author May
+ * <p>
+ * Tomcat 控制模块方法
+ */
+@Slf4j
+public class TomcatControl {
+
+    /**
+     * 重启Tomcat
+     *
+     * @param tomcatLocation tomcat路径
+     */
+    public void rebootTomcat(String tomcatLocation) {
+        core(tomcatLocation);
+    }
+
+    private List<String> core(String location) {
+        List<String> addLine = new ArrayList<>();
+        Runtime rt = Runtime.getRuntime();
+        Process p = null;
+        try {
+            p = rt.exec("cmd /c cd " + location + " & catalina.bat start");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(p).getInputStream()));
+        String line = null;
+        while (true) {
+            try {
+                if ((line = br.readLine()) == null) {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                addLine.add(new String(Objects.requireNonNull(line).getBytes(), "GBK"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return addLine;
+    }
+}
