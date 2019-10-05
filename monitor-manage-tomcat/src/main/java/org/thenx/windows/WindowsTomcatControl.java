e12c84230a6b5499cf594fcfa4574e5fe2cfd0d4
wei_yu <1455755530@qq.com>
2019-10-05 17:43:28 +0800

    fix: 添加 Linux 对Tomcat的操作

--- monitor-manage-tomcat/src/main/java/org/thenx/windows/WindowsTomcatControl.java
+++ monitor-manage-tomcat/src/main/java/org/thenx/windows/WindowsTomcatControl.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,12 +17,6 @@
  * limitations under the License.
  * >>
  */
-
-
-
-
-
-
 
 
 package org.thenx.windows;
@@ -37,8 +31,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * @author May
+ * <p>
+ * Windows 对 Tocmat 的操作
+ */
 @Slf4j
-public class TomcatControl {
+public class WindowsTomcatControl {
 
     public void rebootTomcat(String tomcatLocation) {
         core(tomcatLocation);
